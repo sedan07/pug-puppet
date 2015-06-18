881527e0d4b31b44ff7dbca6b9bd79f3e1b96a7e
Seb Dangerfield <seb.dangerfield@capablue.com>
2015-06-18 16:49:50 +0100

    Removing any trusted fact usage

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,24 +1,2 @@
-# Checking if any trusted facts are defined in the cert, if not we disable this check for now
-# to allow backwards compatibility
-if !is_hash($trusted) {
-
-  crit('Trsuted fact support MUST be turned on the server for any Puppet Catalogs to be served')
-
-} else {
-  if defined('$role') {
-    fail('The servers role can NOT be overridden, it should always be defined by trusted facts')
-  }
-  if defined('$tier') {
-    fail('The servers tier can NOT be overridden, it should always be defined by trusted facts')
-  }
-
-  $role = $trusted['extensions']['1.3.6.1.4.1.34380.1.1.100']
-  $tier = $trusted['extensions']['1.3.6.1.4.1.34380.1.1.101']
-
-  # The $certname fact is self-reported by the node to we cannot trust it :-(
-  # https://docs.puppetlabs.com/puppet/latest/reference/lang_facts_and_builtin_vars.html#puppet-agent-facts
-  $trusted_certname = $trusted['certname']
-
-  hiera_include('classes')
-}
+hiera_include('classes')
 
