4b238a30e1a99f7fe57b89ea87f10ba9f968bada
Seb Dangerfield <seb.dangerfield@capablue.com>
2015-06-18 16:44:04 +0100

    Adding optional wrapper around trusted params

--- manifests/default.pp
+++ manifests/default.pp
@@ -5,15 +5,17 @@ if !is_hash($trusted) {
   crit('Trsuted fact support MUST be turned on the server for any Puppet Catalogs to be served')
 
 } else {
-  if defined('$role') {
-    fail('The servers role can NOT be overridden, it should always be defined by trusted facts')
-  }
-  if defined('$tier') {
-    fail('The servers tier can NOT be overridden, it should always be defined by trusted facts')
-  }
+  unless count($trusted['extensions']) == 0 {
+    if defined('$role') {
+      fail('The servers role can NOT be overridden, it should always be defined by trusted facts')
+    }
+    if defined('$tier') {
+      fail('The servers tier can NOT be overridden, it should always be defined by trusted facts')
+    }
 
-  $role = $trusted['extensions']['1.3.6.1.4.1.34380.1.1.100']
-  $tier = $trusted['extensions']['1.3.6.1.4.1.34380.1.1.101']
+    $role = $trusted['extensions']['1.3.6.1.4.1.34380.1.1.100']
+    $tier = $trusted['extensions']['1.3.6.1.4.1.34380.1.1.101']
+  }
 
   # The $certname fact is self-reported by the node to we cannot trust it :-(
   # https://docs.puppetlabs.com/puppet/latest/reference/lang_facts_and_builtin_vars.html#puppet-agent-facts
